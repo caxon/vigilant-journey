45a737ee5f52ede273eb57e09a1b98c4dba1aeba
tonamuhang <39136949+tonamuhang@users.noreply.github.com>
2020-04-24 04:57:06 -0400

    Update views.py

--- Backend307/gamestate/views.py
+++ Backend307/gamestate/views.py
@@ -18,40 +18,57 @@ def index(request):
 
 @login_required #enforces login (?)
 def room(request, room_id=None):
+    user = None
+    if request.user.is_authenticated:
+        user = request.user
+
     if request.method == 'POST':
         # player1 = escape(request.POST['player1'])
         # player2 = escape(request.POST['player2'])
-        print(request.body)
-        state = json.loads(request.body.decode("utf-8"))
-        player1 = state['player1']
-        player2 = state['player2']
 
-        player1_model = GamePlayer(
-            x=player1['x'],
-            y=player1['y'],
-            z=player1['z']
-        )
-
-        player2_model = GamePlayer(
-            x=player2['x'],
-            y=player2['y'],
-            z=player2['z']
-        )
-
-        players = {'player1': player1_model, 'player2': player2_model}
+        form = forms.GameStateForm(request.POST)
 
 
-        room_name_hash = hash(room_id+0.1)
 
-        game_state = GameLobby(
-            players=players,
-            room_name=room_name_hash
-        )
+        if form.is_valid():
+            player1 = form.cleaned_data['player1']
+            player2 = form.cleaned_data['player2']
 
-        game_state.save()
-        return render(request, '../templates/gamestate/saved.html', {
-            'room_name_hash': room_name_hash
-        })
+            player1 = json.loads(player1)
+            player2 = json.loads(player2)
+            print(player1)
+            print(player2)
+
+            room_name_hash = hash(room_id+0.1)
+
+            game_state = GameLobby(
+                room_name=room_name_hash,
+            )
+            game_state.save()
+
+            player1_model = GamePlayer(
+                x=player1['x'],
+                y=player1['y'],
+                z=player1['z'],
+
+            )
+
+            player2_model = GamePlayer(
+                x=player2['x'],
+                y=player2['y'],
+                z=player2['z']
+            )
+
+            user.players.add(player1_model, bulk = False)
+            user.players.add(player2_model, bulk=False)
+            game_state.players.add(player1_model, bulk=False)
+            game_state.players.add(player2_model, bulk=False)
+            # player1_model.save()
+            # player2_model.save()
+            
+            return render(request, '../templates/gamestate/saved.html', {
+                'room_name_hash': room_name_hash
+            })
 
 
     return render(request, '../templates/gamestate/room.html', {
