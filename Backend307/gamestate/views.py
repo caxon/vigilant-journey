db1cdccd7515030335bef28da09d1a6a94a348a3
Julia Luo <julia.luo98@gmail.com>
2020-04-24 18:45:54 -0400

    score

--- Backend307/gamestate/views.py
+++ Backend307/gamestate/views.py
@@ -34,14 +34,16 @@ def room(request, room_id=None):
         if form.is_valid():
             player1 = form.cleaned_data['player1']
             player2 = form.cleaned_data['player2']
+            # currentScore = form.cleaned_data['currentScore']
 
             player1 = json.loads(player1)
             player2 = json.loads(player2)
             print(player1)
             print(player2)
 
-            # #saving highscore
-            # user.profile.highscore = 10
+            #saving highscore
+            # if currentScore>user.profile.highscore:
+            #     user.profile.highscore = currentScore
             # user.save()
 
             # room_name_hash = hash(room_id+0.1)
